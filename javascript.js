6efc353211a8ba29b83e4afafd9d99de389f5721
planetjelly <286135755+planetjelly@users.noreply.github.com>
2026-07-16 17:31:57 -0400

    Create function to get computer choice Give a random number to the getComputerChoice function and it will return either "Rock", "Paper", or "Scissors"

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
 console.log("Hello, world!")
+
+// step 1 should randomly return "rock", "paper", or "scissors" for the computer choice
+
+function getComputerChoice(randomNumber) {
+    if ( randomNumber < .33 ) {
+        return "Rock"
+    } else if ( randomNumber < .66 ) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+
+let computerChoice = getComputerChoice(Math.random())
+
+
+console.log( computerChoice )
